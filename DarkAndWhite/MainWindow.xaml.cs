279fb09652a1b8f0649e87fd474ff3a479713707
unknown <jn83471@gmail.com>
2024-12-29 21:46:12 -0700

    first commit

---
+++ DarkAndWhite/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DarkAndWhite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WhiteMode_Checked(object sender, RoutedEventArgs e)
+        {
+            app.Default.ColorMode = "White";
+            app.Default.Save();
+        }
+
+        private void LightMode_Checked(object sender, RoutedEventArgs e)
+        {
+            app.Default.ColorMode = "Light";
+            app.Default.Save();
+        }
+
+        private void DarkMode_Checked(object sender, RoutedEventArgs e)
+        {
+            app.Default.ColorMode = "Dark";
+            app.Default.Save();
+        }
+
+        private void BlackMode_Checked(object sender, RoutedEventArgs e)
+        {
+            app.Default.ColorMode = "Black";
+            app.Default.Save();
+        }
+    }
+}
